593d2e512510d3dfd11723e78a5128080b7441ed
Hayley Hollermann <hayleyhollermann@gmail.com>
2020-01-30 17:07:40 -0600

    first commit, hello world

---
+++ src/main/java/com/mycompany/myjavaproject/MyTestClass.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.myjavaproject;
+
+/**
+ *
+ * @author hayley
+ */
+public class MyTestClass {
+    
+    public static void main(String[] args){
+        System.out.println("Hello World!");
+    }
+    
+}
